d2fc360fcf97b6ddb744ff6d24effecfdab3c65e
AfroMonkey <moisalejandro@gmail.com>
2016-12-13 13:16:14 -0600

    [IMP] GitIgnore

--- __openerp__.py
+++ __openerp__.py
@@ -2,9 +2,7 @@
     'name': 'OpenAcademy',
     'version': '1.0',
     'author': 'Moy',
-    'website': 'http://humanytek.com',
     'depends': [
-
     ],  # TODO [IMP]
     'data': [
     ],  # TODO [IMP]
